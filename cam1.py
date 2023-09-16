53e5bc605e5ec71ea3e1ed3c3bf47d3d42695b03
Kev492 <kyj12342000@gmail.com>
2023-09-17 01:16:47 +0900

    first commit

---
+++ cam1.py
@@ -0,0 +1,22 @@
+import cv2
+
+# 웹캠 열기
+cap = cv2.VideoCapture(0)  # 0은 기본 웹캠을 나타냅니다. 다른 카메라를 사용하려면 숫자를 변경하세요.
+
+# 프레임 읽기
+ret, frame = cap.read()
+
+# 테스트 출력
+# cv2.imshow('Webcam Frame', frame)
+# cv2.waitKey(0)
+
+# 이미지 파일로 저장
+save_jpg = "captured_image.jpg"
+cv2.imwrite(save_jpg, frame)
+
+# 작업이 끝나면 웹캠을 닫습니다.
+cap.release()
+cv2.destroyAllWindows()
+
+# 이미지 파일 경로 출력
+print(save_jpg)
