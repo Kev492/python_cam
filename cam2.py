f9f31329d6576c8501bb5b1625a0c694438c5393
Kev492 <kyj12342000@gmail.com>
2023-09-18 02:31:54 +0900

    secomd commit

--- cam2.py
+++ cam2.py
@@ -5,52 +5,48 @@ import numpy as np
 # 웹캠 열기
 cap = cv2.VideoCapture(0)  # 0은 기본 웹캠을 나타냅니다.
 
-ret, frame = cap.read()
 # 이미지 저장 폴더 경로
 save_folder = "C:/pyworkspace1/captured/finger_captured2"
 
-# 폴더가 없다면 생성
-if not os.path.exists(save_folder):
-    os.makedirs(save_folder)
+# Create the save folder if it doesn't exist
+os.makedirs(save_folder, exist_ok=True)
 
-# 손가락 검출을 위한 이미지 처리
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-ret, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+while True:
+    ret, frame = cap.read()
 
-# 가장 큰 윤곽선을 찾아 손가락 영역 추출
-max_area = 0
-finger_frame = None
-for contour in contours:
-    area = cv2.contourArea(contour)
-    if area > max_area:
-        max_area = area
-        x, y, w, h = cv2.boundingRect(contour)
-        finger_frame = frame[y:y+h, x:x+w]
+    # 손가락 검출을 위한 이미지 처리
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    ret, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-if finger_frame is not None:
-   # 손가락 영역의 중심 좌표 계산
-    finger_center_x = w // 2
-    finger_center_y = h // 2
+    # 가장 큰 윤곽선을 찾아 손가락 영역 추출
+    max_area = 0
+    finger_frame = None
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > max_area:
+            max_area = area
+            x, y, w, h = cv2.boundingRect(contour)
+            finger_frame = frame[y:y+h, x:x+w]
 
-    # 원하는 중앙 위치 계산 (예: 화면 중앙)
-    screen_center_x = frame.shape[1] // 2
-    screen_center_y = frame.shape[0] // 2
+    if finger_frame is not None:
+        # 검은 배경 이미지 생성
+        black_background = np.zeros_like(frame)
 
-    # 중앙 위치로 이동하기 위한 변위 계산
-    dx = screen_center_x - (x + finger_center_x)
-    dy = screen_center_y - (y + finger_center_y)
+        # 손가락 영역을 제외한 부분을 검은색으로 채우기 위한 마스크 생성
+        mask = np.ones_like(frame) * 255
+        mask[y:y+h, x:x+w] = 0
 
-    # 이동
-    M = np.float32([[1, 0, dx], [0, 1, dy]])
-    finger_frame = cv2.warpAffine(finger_frame, M, (frame.shape[1], frame.shape[0]))
+        # 손가락 영역을 제외한 배경 생성
+        result_frame = np.where(mask == 0, finger_frame, black_background)
 
-    # 손가락 영역을 파일로 저장
-    image_count = len(os.listdir(save_folder))
-    save_jpg = os.path.join(save_folder, f"captured_image_{image_count}.jpg")
-    cv2.imwrite(save_jpg, finger_frame)
-
+        # 손가락 영역을 파일로 저장 (파일명은 파일 수로 결정)
+        image_count = len(os.listdir(save_folder))
+        save_jpg = os.path.join(save_folder, f"captured_image_{image_count}.jpg")
+        cv2.imwrite(save_jpg, result_frame)
+        break
+    
 # 작업이 끝나면 웹캠을 닫습니다.
 cap.release()
 cv2.destroyAllWindows()
